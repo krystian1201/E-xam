42fc3b64815dd47c090f5f0c02970271dacc9450
krystian1201 <krystian1201@gmail.com>
2015-05-30 11:31:01 +0200

    Just out-of-the-box

---
+++ E-xam/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_xam.Startup))]
+namespace E_xam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
